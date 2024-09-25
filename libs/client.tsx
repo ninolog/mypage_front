60317d645b62ef974f67cfb82396365402af3524
jun <57752066+ninolog@users.noreply.github.com>
2024-09-25 14:37:35 +0900

    Delete libs directory

--- libs/client.tsx
+++
@@ -1,8 +0,0 @@
-// microCMS の API を叩くためのクライアントを作成
-import { createClient } from 'microcms-js-sdk';
-
-// API クライアントを作成
-export const client = createClient({
-  serviceDomain: 'ninolog',  // service-domain は XXXX.microcms.io の XXXX 部分
-  apiKey: 'Lcw0peXCGsaqznhRMMLC2rRZChA6xC8QXbbi',
-});
